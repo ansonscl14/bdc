132c093d3e3bb2ca4a4e9afb46562bc9f9fd4579
ansonscl14 <ansonscl14@gmail.com>
2026-07-21 22:12:06 +0100

    1.0.1

--- binary - decimal.py
+++ binary - decimal.py
@@ -1,5 +1,5 @@
 def r():
-    def b(): 
+    def bi(): 
         try:
             b = input("Enter a binary number: ")
             d = int(b, 2)
@@ -7,11 +7,11 @@ def r():
             print(f"Binary: {b}")
             print(f"Decimal: {d}")
             print(f"Hexadecimal: {h[2:].upper()}")
-            b()
+            bi()
         except ValueError:
             print("That is not a valid binary number!")
             r()
-    def d(): 
+    def de(): 
         try:
             d = int(input("Enter a decimal number: "))
             b = bin(d)
@@ -19,20 +19,20 @@ def r():
             print(f"Binary: {b[2:]}")
             print(f"Decimal: {d}")
             print(f"Hexadecimal: {h[2:].upper()}")
-            d()
+            de()
         except ValueError:
             print("That is not a valid decimal number!")
             r()
 
-    def h(): 
+    def he(): 
         try:
             h = input("Enter a hexadecimal number: ")
             d = int(h, 16)
             b = bin(d)
             print(f"Binary: {b[2:]}")
-            print(f"Decimal: {dl}")
+            print(f"Decimal: {d}")
             print(f"Hexadecimal: {h.upper()}")
-            h()
+            he()
         except ValueError:
             print("That is not a valid hexadecimal number!")
             r()
@@ -42,13 +42,13 @@ def r():
         i = int(input("Choose your input format. Press 1 for Binary, 2 for Decimal, and 3 for Hexadecimal. "))
         if i == 1:
             while True:
-                b()
+                bi()
         elif i == 2:
             while True: 
-                d()
+                de()
         elif i == 3:
             while True: 
-                h()
+                he()
         else: 
             print("Invalid Choice")
             r()
